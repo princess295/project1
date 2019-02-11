bb1146d8f87907b5020041d21cc70b567e5f5083
shelt <shelterly@gmail.com>
2019-02-11 20:25:06 +0300

    =(

--- main.py
+++ main.py
@@ -1,5 +1,4 @@
-import sys
-from PyQt5 import QtWidgets
+from PyQt5 import QtWidgets, QtCore
 from design import Ui_MainWindow
 from calc import Ui_Form
 import os
@@ -10,7 +9,8 @@ class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
         super().__init__()
         self.setupUi(self)
         self.btnBrowse.clicked.connect(self.browse_folder)
-        self.calcButton.clicked.connect()
+        self.calc = CCalculator(self)
+        self.calcButton.clicked.connect(self.calc.show)
     def browse_folder(self):
         self.listWidget.clear()
         directory = QtWidgets.QFileDialog.getExistingDirectory(self, "Выберите папку")
@@ -19,11 +19,64 @@ class ExampleApp(QtWidgets.QMainWindow, Ui_MainWindow):
                 self.listWidget.addItem(file_name)
 
 class CCalculator(QtWidgets.QWidget, Ui_Form):
-    def __init__(self):
+    def __init__(self, parent):
         super().__init__()
+        self.result =''
         self.setupUi(self)
-
-
+        self.oneButton.clicked.connect(self.setOneButton)
+        self.twoButton.clicked.connect(self.setTwoButton)
+        self.threeButton.clicked.connect(self.setThreeButton)
+        self.fourButton.clicked.connect(self.setFourButton)
+        self.fiveButton.clicked.connect(self.setFiveButton)
+        self.sixButton.clicked.connect(self.setSixButton)
+        self.sevenButton.clicked.connect(self.setSevenButton)
+        self.eightButton.clicked.connect(self.setEightButton)
+        self.nineButton.clicked.connect(self.setNineButton)
+        self.zeroButton.clicked.connect(self.setZeroButton)
+        self.zzButton.clicked.connect(self.setZZButton)
+        self.plusButton.clicked.connect(self.setPlusButton)
+        self.minusButton.clicked.connect(self.setMinusButton)
+        self.umnButton.clicked.connect(self.setUmnButton)
+        self.delenButton.clicked.connect(self.setDelenButton)
+        self.deleteButton.clicked.connect(self.setDeleteButton)
+        self.procentButton.clicked.connect(self.setProcentButton)
+        self.ravnoButton.clicked.connect(self.setRavnoButton)
+        self.clearButton.clicked.connect(self.setClearButton)
+    def setOneButton(self):
+        self.result += '1'
+        self.lineEdit.setText(self.result)
+    def setTwoButton(self):
+        self.result += '2'
+        self.lineEdit.setText(self.result)
+    def setThreeButton(self):
+        self.result += '3'
+        self.lineEdit.setText(self.result)
+    def setFourButton(self):
+        self.result += '4'
+        self.lineEdit.setText(self.result)
+    def setFiveButton(self):
+        self.result += '5'
+        self.lineEdit.setText(self.result)
+    def setSixButton(self):
+        self.result += '6'
+        self.lineEdit.setText(self.result)
+    def setSevenButton(self):
+        self.result += '7'
+        self.lineEdit.setText(self.result)
+    def setEightButton(self):
+        self.result += '8'
+        self.lineEdit.setText(self.result)
+    def setNineButton(self):
+        self.result += '9'
+        self.lineEdit.setText(self.result)
+    def setZeroButton(self):
+        self.result += '0'
+        self.lineEdit.setText(self.result)
+    def setZZButton(self):
+        self.result += '00'
+        self.lineEdit.setText(self.result)
+    def setClearButton(self):
+        self.lineEdit.clear(self)
 
 sys._excepthook = sys.excepthook
 def my_exception_hook(exctype,value,traceback):
@@ -31,7 +84,6 @@ def my_exception_hook(exctype,value,traceback):
     sys._excepthook(exctype, value, traceback)
     sys.exit(1)
 sys.excepthook = my_exception_hook
-
 def main():
     app = QtWidgets.QApplication(sys.argv)
     window = ExampleApp()
@@ -40,9 +92,5 @@ def main():
         sys.exit(app.exec_())
     except:
         print("exit")
-
 if __name__ == '__main__':
     main()
-
-
-
